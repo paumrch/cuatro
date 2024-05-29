e89d5e31eab074a82f0bd05ffb680e8f20d8dc86
Pau March <paumrch94@gmail.com>
2024-05-29 15:46:28 +0200

    Changes

--- src/components/AnimatedElement.jsx
+++ src/components/AnimatedElement.jsx
@@ -9,13 +9,19 @@ const AnimatedElement = ({ children, animation, delay = 0 }) => {
   useEffect(() => {
     const element = elementRef.current;
 
-    gsap.set(element, { opacity: 0, visibility: 'hidden' });
+    // Asegúrate de que el elemento tiene la clase `hidden` al renderizar
+    element.classList.add('hidden');
 
     if (animation) {
+      // Utiliza GSAP para quitar la clase `hidden` y aplicar la animación
       gsap.fromTo(
         element,
-        { opacity: 0, visibility: 'visible' },
-        { ...animation, opacity: 1, delay }
+        { opacity: 0 },
+        { ...animation, opacity: 1, delay,
+          onStart: () => {
+            element.classList.remove('hidden');
+          }
+        }
       );
     }
   }, [animation, delay]);
@@ -28,4 +34,3 @@ const AnimatedElement = ({ children, animation, delay = 0 }) => {
 };
 
 export default AnimatedElement;
-
